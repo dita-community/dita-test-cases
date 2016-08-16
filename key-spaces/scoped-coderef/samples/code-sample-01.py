c50d0c0593003905064dee03540ee068549a788d
Eliot Kimber <ekimber@contrext.com>
2016-08-16 11:47:04 -0500

    New test case for scoped coderefs

---
+++ key-spaces/scoped-coderef/samples/code-sample-01.py
@@ -0,0 +1,4 @@
+/* This is python file code-sample-01.py 
+*/
+function func01
+   println "Code Sample 01"
---
+++ key-spaces/scoped-coderef/samples/code-sample-02.py
@@ -0,0 +1,4 @@
+/* This is python file code-sample-02.py 
+*/
+function func02
+   println "Code Sample 02"
